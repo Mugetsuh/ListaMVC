7c44151b4bbf471feae9db2217dcc691654c8ab1
Julian David <juliandavid677@gmail.com>
2019-08-14 21:17:43 -0500

    datos en lista
    Ya se muestran los datos en la lista

--- src/Controller/controllerList.java
+++ src/Controller/controllerList.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
+import javax.swing.DefaultListModel;
 
 /**
  *
@@ -17,12 +18,16 @@ public class controllerList implements ActionListener {
     private encaVariables variable;
     private modelList modC;
     private viewFrame frm;
+    DefaultListModel modelolista;
 
     public controllerList(encaVariables variable, modelList modC, viewFrame frm) {
         this.variable = variable;
         this.modC = modC;
         this.frm = frm;
         this.frm.btn_agregar.addActionListener(this);
+        modelolista = new DefaultListModel();  
+        frm.jlist_valores.setModel(modelolista);        
+        
     }
 
     public void iniciar() {
@@ -34,11 +39,13 @@ public class controllerList implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == frm.btn_agregar) {
             variable.setVaraible(frm.txt_valor.getText());
+            String a = frm.txt_valor.getText();
             if (modC.insertar(variable)) {
                 JOptionPane.showMessageDialog(null, "Registro Guardado");
             } else {
                 JOptionPane.showMessageDialog(null, "Error al Guardar");
             }
+            modelolista.addElement(a);
         }
     }
 }
